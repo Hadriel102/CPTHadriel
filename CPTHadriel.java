6fb2d0bb88809775ea68fd7a983d825aee877da9
Hadriel102 <159478646+Hadriel102@users.noreply.github.com>
2024-12-19 09:28:16 -0500

    Create CPTHadriel.java

---
+++ CPTHadriel.java
@@ -0,0 +1,24 @@
+import arc.*;
+
+public class CPTHadriel{
+	public static void main(String[] args){
+		Console con = new Console();
+		
+		// Variables
+		int intDeck[][];
+		int intValue;
+		int intSuite;
+		int intCount;
+		
+		// Creating array	
+		intDeck = new int[52][3];
+		
+		// Assigning Values
+		for(intCount = 0; intCount <= 52; intCount++){
+			intDeck[intCount][0] = intValue; 
+		}
+		for(intCount = 0; intCount <= 52; intCount++){
+			intDeck[intCount][1] = intSuite;
+		}
+	}
+}
