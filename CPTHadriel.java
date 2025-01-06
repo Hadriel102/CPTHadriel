cc2350318e72bfc2c7aed5e8bc2c75719921e4ac
Hadriel102 <159478646+Hadriel102@users.noreply.github.com>
2025-01-06 10:37:24 -0500

    Update CPTHadriel.java
    1. Started to get input from user
    2. Generated random number for column 3
    3. Created Array for Player and Dealer

--- CPTHadriel.java
+++ CPTHadriel.java
@@ -6,11 +6,17 @@ public class CPTHadriel{
 		
 		// Variables
 		int intDeck[][];
+		int intPlayer[][];
+		int intDealer[][];
 		int intValue;
 		int intSuite;
 		int intCount;
+		String strName;
+		int intMoney = 1000;
+		int intBet;
+		int intRand = (int)(Math.random() * 100 + 1);
 		
-		// Creating array	
+		// Array for deck
 		intDeck = new int[52][3];
 		
 		// Assigning Values
@@ -20,5 +26,20 @@ public class CPTHadriel{
 		for(intCount = 0; intCount <= 52; intCount++){
 			intDeck[intCount][1] = intSuite;
 		}
+		for(intCount = 0; intCount <= 52; intCount++){
+			intDeck[intCount][2] = intRand;
+		}
+		
+		// Input
+		con.print("Enter your username: ");
+		strName = con.readLine();
+		con.print("Enter bet amount: ");
+		intBet = con.readInt();
+		
+		// Array for play
+		intPlayer = new int[5][2];
+		intDealer = new int[5][2];
+		
+		
 	}
 }
