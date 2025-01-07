bfc7df8ca8627ef5f978380c611e63c8b02664e0
Hadriel102 <159478646+Hadriel102@users.noreply.github.com>
2025-01-07 16:08:39 -0500

    Update CPTHadriel.java
    1. Fixed assigning the values of the cards
    2. Started to work on sorting / shuffling the cards

--- CPTHadriel.java
+++ CPTHadriel.java
@@ -21,14 +21,40 @@ public class CPTHadriel{
 		
 		// Assigning Values
 		for(intCount = 0; intCount <= 52; intCount++){
-			intDeck[intCount][0] = intValue; 
+			for(intValue = 0; intValue <= 13; intValue++){
+				intDeck[intCount][0] = intValue; 
+			}
 		}
 		for(intCount = 0; intCount <= 52; intCount++){
-			intDeck[intCount][1] = intSuite;
+			for(intSuite = 0; intSuite <= 4; intSuite++){
+				intDeck[intCount][1] = intSuite;
+			}
 		}
 		for(intCount = 0; intCount <= 52; intCount++){
 			intDeck[intCount][2] = intRand;
 		}
+		
+		// Sort
+		for (intRand = 0; intRand < 3 - 1; intRand++) {
+			for (intValue = 0; intValue < 3 - 1 - intRand; intValue++) {
+				// Compare the prices (removing the "$" sign)
+				if(intDeck[intRand][2] > intDeck[intRand + 1][2]){
+					// Take Left Item
+					intValue = intDeck[intValue][0];
+					intRand = intDeck[intValue][2];
+					// Right Item Moves To The Left
+					intDeck[intValue][0] = intDeck[intValue + 1][0];
+					intDeck[intValue][2] = intDeck[intValue + 1][2];
+					// Put Temporary Value On The Right
+					intDeck[intValue + 1][0] = intValue;						
+					intDeck[intValue + 1][2] = intRand;
+				}
+				con.println(intValue);
+				con.println(intRand);
+				con.sleep(1000);
+			}
+		}
+		
 		
 		// Input
 		con.print("Enter your username: ");
