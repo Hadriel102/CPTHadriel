9eb85ea30a2a432629066b3fe7c9e2911a363759
Hadriel102 <159478646+Hadriel102@users.noreply.github.com>
2025-01-09 21:51:22 -0500

    Update CPTHadriel.java
    1. Attempted to change the sorting into a method
    2. Finishing requirements and UX Documents

--- CPTHadriel.java
+++ CPTHadriel.java
@@ -2,7 +2,7 @@ import arc.*;
 
 public class CPTHadriel{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("Blackjack", 1280, 720);
 		
 		// Variables
 		int intDeck[][];
@@ -16,57 +16,22 @@ public class CPTHadriel{
 		// Array for deck
 		intDeck = new int[52][3];
 		
-		// Assigning Values
-		for(intCount = 0; intCount < 52; intCount++){
-			intDeck[intCount][0] = (intCount % 13) + 1;
-			if(intDeck[intCount][0] > 10){
-				intDeck[intCount][0] = 10;
-			}
-			intDeck[intCount][1] = (intCount / 13) + 1;
-			intDeck[intCount][2] = (int)(Math.random() * 100 + 1);
-		}
-		
-		// Create the variables for the sorting
-		int intTempVal;
-		int intTempSuite;
-		int intTempRand;
-		int intRow;
-		int intRow2;
-		
-		// Loop every single card so it uses bubble sorting and sorts the random numbers from lowest to highest
-		for(intRow2 = 0; intRow2 < 52 - 1; intRow2++){
-			for(intRow = 0; intRow < 52 - 1- intRow2; intRow++){
-				if(intDeck[intRow][2] > intDeck[intRow + 1][2]){
-					// Take the left item
-					intTempVal = intDeck[intRow][0];
-					intTempSuite = intDeck[intRow][1];
-					intTempRand = intDeck[intRow][2];
-					// Right item moves to the left
-					intDeck[intRow][0] = intDeck[intRow+1][0];
-					intDeck[intRow][1] = intDeck[intRow+1][1];
-					intDeck[intRow][2] = intDeck[intRow+1][2];
-					// Put temporary value on the right
-					intDeck[intRow+1][0] = intTempVal;
-					intDeck[intRow+1][1] = intTempSuite;
-					intDeck[intRow+1][2] = intTempRand;
-				}
-			}
-		}
-		
-
-		
-		
-		
 		// Input
 		con.print("Enter your username: ");
 		strName = con.readLine();
 		con.print("Enter bet amount: ");
 		intBet = con.readInt();
 		
+		if(intBet > 0){
+			hadrieltoolsCPT.shuffleDeck();
+		}
+		
 		// Array for play
 		intPlayer = new int[5][2];
 		intDealer = new int[5][2];
 		
 		
+		
+
 	}
 }
